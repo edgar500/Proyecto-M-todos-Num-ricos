0f59335b1749e682aa81c94a8f6bcab37e3ec1c1
Edgar <420086734@pcpuma.acatlan.unam.mx>
2021-10-31 20:46:00 -0600

    +Dirección de libreria Jacobi.h

--- main/main.cpp
+++ main/main.cpp
@@ -1,12 +1,37 @@
 
 #include<iostream>
+#include<conio.h>
+#include<stdlib.h>
 #include "C:\Users\hp\Documents\Semestre 2022-1\Metodos numericos I\Proyecto Métodos Numéricos\Librerias\M. Newton\Newton.h"
+#include "C:\Users\hp\Documents\Semestre 2022-1\Metodos numericos I\Proyecto Métodos Numéricos\Librerias\M.Jacobi\Jacobi.h"
 using namespace std;
 
 int main(){
 	
-	Newton newton;
-	newton.muestra();
+	int menu, op;
+	
+	menu=1;
+	while(menu==1){
+		
+		cout<<"\n\n\t ====== Elija un metodo ====== \n\n";
+		
+		cout<<"1 -> Newton \n\n "<<"2 -> Jacobi \n\n";
+		cout<<"-> Opcion: ";
+		cin>>op;
+		
+		if(op==1){
+			
+			Newton newton;
+	        newton.muestra();
+		}
+		
+		if(op==2){
+			
+			Jacobi jacobi;
+			jacobi.setJacobi();
+			jacobi.getJacobi();
+		}
+	}
 	
 	system("pause");
 	return 0;
